a490c027ce680ca3fa2f8d503fe996ffb1e036fe
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-25 09:09:11 -0400

    commit tree 4dc85437332e8ea99972fbb85c18970cbf75ed78

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  let arr = word.split('').reverse().join('');
+
+  return arr === word;
 }
 
 /* 
   Add your pseudocode here
+  - make each letter an array
+  - reverse the array
+  - convert the array to a string
+
+  if the converted string is equal to the argument
+  : return true;
+  else: return false;
 */
 
 /*
   Add written explanation of your solution here
+    - make each letter an array
+    - reverse the array
+    - convert the array to a string
 */
 
 // You can run `node index.js` to view these console logs
